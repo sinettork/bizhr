ad0d9f7d7ead345e47d22b5a0e8616f15ffb9b2c
sreyhoeurnouch-byte <sreyhoeurnouch@gmail.com>
2026-08-18 20:57:49 +0700

    Add reusable approval decision actions

---
+++ resources/views/components/decision-actions.blade.php
@@ -0,0 +1,26 @@
+@props([
+    'approveUrl',
+    'rejectTarget',
+    'approveLabel' => 'Approve',
+    'rejectLabel' => 'Reject',
+    'approveIcon' => 'fa-check',
+    'rejectIcon' => 'fa-xmark',
+    'compact' => true,
+    'approveFields' => [],
+])
+
+<div {{ $attributes->class(['d-flex flex-wrap gap-2 decision-actions']) }}>
+    <button class="btn btn-outline-danger {{ $compact ? 'btn-sm' : '' }}" type="button" data-bs-toggle="modal" data-bs-target="{{ $rejectTarget }}">
+        <i class="fa-solid {{ $rejectIcon }} me-1"></i>{{ $rejectLabel }}
+    </button>
+
+    <form method="POST" action="{{ $approveUrl }}">
+        @csrf
+        @foreach($approveFields as $name => $value)
+            <input type="hidden" name="{{ $name }}" value="{{ $value }}">
+        @endforeach
+        <button class="btn btn-success {{ $compact ? 'btn-sm' : '' }}" type="submit">
+            <i class="fa-solid {{ $approveIcon }} me-1"></i>{{ $approveLabel }}
+        </button>
+    </form>
+</div>
