52cf818596bc7815d00188b32697b2cc679dd47d
sreyhoeurnouch-byte <sreyhoeurnouch@gmail.com>
2026-08-18 14:18:57 +0700

    Load application shell styles

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,3 +8,4 @@
 <link href="{{ asset('vendor/fontawesome/css/all.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/status.css') }}" rel="stylesheet">
+<link href="{{ asset('css/app-shell.css') }}" rel="stylesheet">
