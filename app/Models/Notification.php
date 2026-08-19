3d2a36122997f45142d62c02db295a05cb60d139
sreyhoeurnouch-byte <sreyhoeurnouch@gmail.com>
2026-08-19 13:58:02 +0700

    Fix notification Pint formatting

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -53,7 +53,7 @@ class Notification extends Model
 
     public function markAsRead(): void
     {
-        if (!$this->is_read) {
+        if (! $this->is_read) {
             $this->update([
                 'is_read' => true,
                 'read_at' => now(),
